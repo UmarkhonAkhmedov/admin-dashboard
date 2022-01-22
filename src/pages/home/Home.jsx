7b2568a592ddef0122708df32ef86de656868bfb
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-23 04:48:46 +0500

    Adding user analytics chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../featuredInfo/FeaturedInfo";
 import "./home.css"
+import {userData} from "../../dummyData"
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo/>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   );
 }
