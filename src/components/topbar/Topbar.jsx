bc5b07a3c495db193f4a8efe6a1379793f6cfe76
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-23 01:35:19 +0500

    Adding navbar menu component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import "./topbar.css"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Topbar() {
   return (
-    <div>
-    
+    <div className='topbar'>
+      <div className='topbarWrapper'>
+        <div className="topLeft">
+          <span className="logo">lamaadmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon/>
+            <span className='topIconBadge'>2</span>
+          </div><div className="topbarIconContainer">
+            <LanguageIcon/>
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon/>
+          </div>
+          <img src="https://avatars.githubusercontent.com/u/76143018?v=4" alt="" className="topAvatar" />
+        </div>
+      </div>
     </div>
   );
 }
