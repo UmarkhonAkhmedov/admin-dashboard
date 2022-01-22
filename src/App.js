686bef99bf219f95d465b37103510a00ad09743f
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-23 04:21:03 +0500

    Adding Featured Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Topbar from "./components/topbar/Topbar"
 import Sidebar from "./components/sidebar/Sidebar"
 import "./App.css"
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar/>
+        <Home/>
       </div>
     </div>
   );
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import FeaturedInfo from "../featuredInfo/FeaturedInfo";
+import "./home.css"
+
+function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo/>
+    </div>
+  );
+}
+
+export default Home;
+
