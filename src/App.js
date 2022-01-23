7ff1d5fea660fca57db39154edb73b900b2f5814
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-24 02:47:58 +0500

    Adding User Table Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import Topbar from "./components/topbar/Topbar"
 import Sidebar from "./components/sidebar/Sidebar"
 import "./App.css"
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+import UserList from "./pages/userList/UserList";
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+          <Route exact path="/" exact element={<Home/>}/>
+          <Route path="/users" exact element={<UserList/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,17 @@ export const userData = [
     "Active User": 3000,
   },
 ];
+
+export const userRows = [
+  { id: 1, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 2, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 3, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 4, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 5, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 6, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transation: "$120.00" },
+  { id: 7, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 8, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 9, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  { id: 10, username: 'John Snow', avatar: "https://i.insider.com/5ea9e4facdfd480f0125a2ca?width=700", email: "john@gmail.com", status: "active", transaction: "$120.00" },
+  
+];
